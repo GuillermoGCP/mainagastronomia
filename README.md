077b59e2474c66a961e7b1f5c3a458b601724371
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-11-06 17:02:56 +0100

    Separador footer. Página de menus de grupo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# mainagastronomia
+# Mainagastronomia
+
+**Developer**: [Guillermo Cerviño Porto](https://www.linkedin.com/in/guillermocporto/)
+
+**Tech Stack**:
+
+-   Vanilla JavaScript
+-   HTML5
+-   CSS3
--- script.js
+++ script.js
@@ -2,13 +2,16 @@ function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu')
     navMenu.classList.toggle('active')
     const tableContainer = document.querySelector('.table-container')
+    const menuContainer = document.querySelector('.menus-container')
 
     if (navMenu.classList.contains('active')) {
         navMenu.style.display = 'flex' // Muestra el menú
-        tableContainer.style.margin = '75px auto 0' //Deja sitio para el deplegable en la sección de reservas
+        if (tableContainer) tableContainer.style.margin = '105px auto 0' //Deja sitio para el deplegable en la sección de reservas
+        if (menuContainer) menuContainer.style.margin = '105px auto 0'
     } else {
         navMenu.style.display = 'none' // Oculta el menú
-        tableContainer.style.margin = '0 auto '
+        if (tableContainer) tableContainer.style.margin = '0 auto '
+        if (menuContainer) menuContainer.style.margin = '0 auto'
     }
 }
 
@@ -30,7 +33,9 @@ document.addEventListener('click', (event) => {
 window.addEventListener('resize', () => {
     const navMenu = document.querySelector('.nav-menu')
     const tableContainer = document.querySelector('.table-container')
-    tableContainer.style.margin = '0 auto '
+    const menuContainer = document.querySelector('.menus-container')
+    if (tableContainer) tableContainer.style.margin = '0 auto '
+    if (menuContainer) menuContainer.style.margin = '0 auto '
     if (window.innerWidth >= 768) {
         navMenu.style.display = 'flex'
         navMenu.classList.remove('active')
