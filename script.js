c70214bf01faa31ad7cb6556feb87133e3d5e888
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-04-01 11:20:55 +0200

    Actualizaciones en la web

--- script.js
+++ script.js
@@ -1,69 +1,72 @@
 function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu')
-    navMenu.classList.toggle('active')
-    const tableContainer = document.querySelector('.table-container')
-    const menuContainer = document.querySelector('.menus-container')
+  const navMenu = document.querySelector('.nav-menu')
+  navMenu.classList.toggle('active')
+  const tableContainer = document.querySelector('.table-container')
+  const menuContainer = document.querySelector('.menus-container')
+  const mainSection = document.querySelector('.main-section')
+  const instagramContainer = document.querySelector('.instagram-container')
 
-    if (navMenu.classList.contains('active')) {
-        navMenu.style.display = 'flex' // Muestra el menú
-        if (tableContainer) tableContainer.style.margin = '105px auto 0' //Deja sitio para el deplegable en la sección de reservas
-        if (menuContainer) menuContainer.style.margin = '105px auto 0'
-    } else {
-        navMenu.style.display = 'none' // Oculta el menú
-        if (tableContainer) tableContainer.style.margin = '0 auto '
-        if (menuContainer) menuContainer.style.margin = '0 auto'
-    }
+  if (navMenu.classList.contains('active')) {
+    navMenu.style.display = 'flex' // Muestra el menú
+    if (tableContainer) tableContainer.style.margin = '160px auto 0' //Deja sitio para el deplegable en la sección de reservas
+    if (menuContainer) menuContainer.style.margin = '130px auto 0'
+    if (mainSection) mainSection.style.margin = '130px auto 0'
+    if (instagramContainer) instagramContainer.style.margin = '130px auto 0'
+  } else {
+    navMenu.style.display = 'none' // Oculta el menú
+    if (tableContainer) tableContainer.style.margin = '0 auto '
+    if (menuContainer) menuContainer.style.margin = '0 auto'
+    if (mainSection) mainSection.style.margin = '0 auto'
+    if (instagramContainer) instagramContainer.style.margin = '0 auto'
+  }
 }
 
 document.addEventListener('click', (event) => {
-    const navMenu = document.querySelector('.nav-menu')
-    const menuIcon = document.querySelector('.menu-icon')
+  const navMenu = document.querySelector('.nav-menu')
+  const menuIcon = document.querySelector('.menu-icon')
 
-    if (window.innerWidth < 768) {
-        if (
-            !navMenu.contains(event.target) &&
-            !menuIcon.contains(event.target)
-        ) {
-            navMenu.classList.remove('active')
-            navMenu.style.display = 'none'
-            const tableContainer = document.querySelector('.table-container')
-            const menuContainer = document.querySelector('.menus-container')
-            if (tableContainer) tableContainer.style.margin = '0 auto '
-            if (menuContainer) menuContainer.style.margin = '0 auto '
-        }
+  if (window.innerWidth < 768) {
+    if (!navMenu.contains(event.target) && !menuIcon.contains(event.target)) {
+      navMenu.classList.remove('active')
+      navMenu.style.display = 'none'
+      const tableContainer = document.querySelector('.table-container')
+      const menuContainer = document.querySelector('.menus-container')
+      if (tableContainer) tableContainer.style.margin = '0 auto '
+      if (menuContainer) menuContainer.style.margin = '0 auto '
     }
+  }
 })
 
 window.addEventListener('resize', () => {
-    const navMenu = document.querySelector('.nav-menu')
-    const tableContainer = document.querySelector('.table-container')
-    const menuContainer = document.querySelector('.menus-container')
-    if (tableContainer) tableContainer.style.margin = '0 auto '
-    if (menuContainer) menuContainer.style.margin = '0 auto '
-    if (window.innerWidth >= 768) {
-        navMenu.style.display = 'flex'
-        navMenu.classList.remove('active')
-    } else {
-        navMenu.style.display = 'none'
-        navMenu.classList.remove('active')
-    }
+  const navMenu = document.querySelector('.nav-menu')
+  const tableContainer = document.querySelector('.table-container')
+  const menuContainer = document.querySelector('.menus-container')
+  if (tableContainer) tableContainer.style.margin = '0 auto '
+  if (menuContainer) menuContainer.style.margin = '0 auto '
+  if (window.innerWidth >= 768) {
+    navMenu.style.display = 'flex'
+    navMenu.classList.remove('active')
+  } else {
+    navMenu.style.display = 'none'
+    navMenu.classList.remove('active')
+  }
 })
 
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-    })
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
 }
 
 window.addEventListener('scroll', () => {
-    const scrollButton = document.querySelector('.scroll-to-top')
-    if (
-        document.body.scrollTop > 100 ||
-        document.documentElement.scrollTop > 100
-    ) {
-        scrollButton.classList.add('visible')
-    } else {
-        scrollButton.classList.remove('visible')
-    }
+  const scrollButton = document.querySelector('.scroll-to-top')
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollButton.classList.add('visible')
+  } else {
+    scrollButton.classList.remove('visible')
+  }
 })
